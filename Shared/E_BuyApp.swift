0d5e2d8f815648bd35c0e1de230d299e252a609f
MacBook <macbook@MacBooks-MacBook-Air.local>
2024-02-23 09:09:00 +0100

    Initial commit message

---
+++ Shared/E_BuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_BuyApp.swift
+//  Shared
+//
+//  Created by MacBook on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct E_BuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
