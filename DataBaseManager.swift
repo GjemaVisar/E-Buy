7b12359f5aedd4cb30add0b3e7a172ed96d0c6bf
GjemaVisar <visar.gjema@student.uni-pr.edu>
2024-02-24 10:19:59 +0100

    BackEnd/DataBaseManager/CreatedAndTestedTable

--- DataBaseManager.swift
+++ DataBaseManager.swift
@@ -6,6 +6,14 @@ class DataBaseManager {
     
     private var db: OpaquePointer?
     
+    private let productTable = "product"
+    
+    private let nameColumn = "name"
+    private let priceColumn = "price"
+    private let imageColumn = "image"
+    private let descriptionColumn = "description"
+    private let quantityColumn = "quantity"
+
     private init() {
         openDatabase()
     }
@@ -16,10 +24,13 @@ class DataBaseManager {
     
     private func openDatabase() {
         if db != nil {
+            print("Database already opened")
+            createTable()
             return // Database already opened
         }
         
         if sqlite3_open(getDBPath(), &db) == SQLITE_OK {
+            createTable()
             print("Database opened successfully")
         } else {
             print("Error opening database")
@@ -28,86 +39,104 @@ class DataBaseManager {
     
     private func getDBPath() -> String {
         let documentsDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        let dbPath = URL(fileURLWithPath: documentsDir).appendingPathComponent("moviesDB.sqlite").path
+        let dbPath = URL(fileURLWithPath: documentsDir).appendingPathComponent("shopDB.sqlite").path
         return dbPath
     }
     
-//    private func createTable() {
-//        let createTableQuery = """
-//        CREATE TABLE IF NOT EXISTS Movies (
-//            id INTEGER PRIMARY KEY AUTOINCREMENT,
-//            title TEXT,
-//            year TEXT,
-//            imdbID TEXT,
-//            type TEXT,
-//            poster TEXT
-//        );
-//        """
-//
-//        if sqlite3_exec(db, createTableQuery, nil, nil, nil) == SQLITE_OK {
-//            print("Table created successfully")
-//        } else {
-//            print("Error creating table")
-//        }
-//    }
+    private func createTable() {
+        let createTableQuery = """
+        CREATE TABLE IF NOT EXISTS \(productTable) (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            \(nameColumn) TEXT NOT NULL,
+            \(priceColumn) REAL NOT NULL,
+            \(imageColumn) TEXT NOT NULL,
+            \(descriptionColumn) TEXT NOT NULL,
+            \(quantityColumn) INTEGER NOT NULL
+        );
+        """
+
+        if sqlite3_exec(db, createTableQuery, nil, nil, nil) == SQLITE_OK {
+            print("Table created successfully")
+        } else {
+            print("Error creating table")
+        }
+    }
     
-//    func insertMovie(movie: Movie) -> Bool {
-//        let insertQuery = """
-//        INSERT INTO Movies (title, year, imdbID, type, poster)
-//        VALUES (?, ?, ?, ?, ?);
-//        """
-//
-//        var statement: OpaquePointer?
-//
-//        if sqlite3_prepare_v2(db, insertQuery, -1, &statement, nil) == SQLITE_OK {
-//            defer {
-//                sqlite3_finalize(statement)
-//            }
-//
-//            sqlite3_bind_text(statement, 1, (movie.Title as NSString).utf8String, -1, nil)
-//            sqlite3_bind_text(statement, 2, (movie.Year as NSString).utf8String, -1, nil)
-//            sqlite3_bind_text(statement, 3, (movie.imdbID as NSString).utf8String, -1, nil)
-//            sqlite3_bind_text(statement, 4, (movie._Type as NSString).utf8String, -1, nil)
-//            sqlite3_bind_text(statement, 5, (movie.Poster as NSString).utf8String, -1, nil)
-//
-//            if sqlite3_step(statement) == SQLITE_DONE {
-//                print("Movie added to the database successfully.")
-//                return true
-//            } else {
-//                print("Error inserting movie into the database")
-//            }
-//        } else {
-//            print("Error preparing insert statement")
-//        }
-//
-//        return false
-//    }
-//
-//    func removeMovie(imdbID: String) -> Bool {
-//        let deleteQuery = """
-//        DELETE FROM Movies
-//        WHERE imdbID = ?;
-//        """
-//
-//        var statement: OpaquePointer?
-//
-//        if sqlite3_prepare_v2(db, deleteQuery, -1, &statement, nil) == SQLITE_OK {
-//            defer {
-//                sqlite3_finalize(statement)
-//            }
-//
-//            sqlite3_bind_text(statement, 1, (imdbID as NSString).utf8String, -1, nil)
-//
-//            if sqlite3_step(statement) == SQLITE_DONE {
-//                print("Movie removed from the database successfully.")
-//                return true
-//            } else {
-//                print("Error deleting movie from the database")
-//            }
-//        } else {
-//            print("Error preparing delete statement")
-//        }
-//
-//        return false
-//    }
-}
+    func insertProduct(name: String, price: Double, image: String?, description: String?, quantity: Int?) {
+        let insertQuery = """
+            INSERT INTO \(productTable) (\(nameColumn), \(priceColumn), \(imageColumn), \(descriptionColumn), \(quantityColumn))
+            VALUES (?, ?, ?, ?, ?);
+        """
+
+        var statement: OpaquePointer?
+
+        if sqlite3_prepare_v2(db, insertQuery, -1, &statement, nil) == SQLITE_OK {
+            defer {
+                sqlite3_finalize(statement)
+            }
+
+            sqlite3_bind_text(statement, 1, (name as NSString).utf8String, -1, nil)
+            sqlite3_bind_double(statement, 2, price)
+
+            if let image = image {
+                sqlite3_bind_text(statement, 3, (image as NSString).utf8String, -1, nil)
+            } else {
+                sqlite3_bind_null(statement, 3)
+            }
+
+            if let description = description {
+                sqlite3_bind_text(statement, 4, (description as NSString).utf8String, -1, nil)
+            } else {
+                sqlite3_bind_null(statement, 4)
+            }
+
+            if let quantity = quantity {
+                sqlite3_bind_int(statement, 5, Int32(quantity))
+            } else {
+                sqlite3_bind_null(statement, 5)
+            }
+
+            if sqlite3_step(statement) == SQLITE_DONE {
+                print("Product inserted successfully.")
+            } else {
+                print("Error inserting product into the database")
+            }
+        } else {
+            print("Error preparing insert statement")
+        }
+    }
+
+    public func insertDummieData() {
+        insertProduct(name: "Product1", price: 19.99, image: "product1.jpg", description: "Description of Product1", quantity: 10)
+        insertProduct(name: "Product2", price: 29.99, image: nil, description: nil, quantity: nil)
+        insertProduct(name: "Product3", price: 39.99, image: nil, description: nil, quantity: 5)
+        print("Data inserted succesfully")
+        // Add more dummy data as needed
+    }
+    
+    func printAllProducts() {
+          let query = "SELECT * FROM \(productTable);"
+
+          var statement: OpaquePointer?
+
+          if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
+              defer {
+                  sqlite3_finalize(statement)
+              }
+
+              while sqlite3_step(statement) == SQLITE_ROW {
+                  let id = sqlite3_column_int(statement, 0)
+                  let name = String(cString: sqlite3_column_text(statement, 1))
+                  let price = sqlite3_column_double(statement, 2)
+                  let image = String(cString: sqlite3_column_text(statement, 3))
+                  let description = String(cString: sqlite3_column_text(statement, 4))
+                  let quantity = sqlite3_column_int(statement, 5)
+
+                  print("ID: \(id), Name: \(name), Price: \(price), Image: \(image), Description: \(description), Quantity: \(quantity)")
+              }
+          } else {
+              print("Error preparing SELECT statement")
+          }
+      }
+  }
+
