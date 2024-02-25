12ee339da2966fc88aed6012e0051b0758f92147
rigonOs <osmanajrigon3@gmail.com>
2024-02-25 11:55:56 +0100

    UI/HomePageController/AddedProductsInTableView

--- HomaPageController.swift
+++ HomaPageController.swift
@@ -1,37 +1,38 @@
 import UIKit
+import SDWebImage
 
-class HomePageController: UIViewController, UITableViewDataSource{
+class HomePageController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var productsTableView: UITableView!
     
     private var products: [DataBaseManager.Product] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         productsTableView.dataSource = self
-       
-       
-        // To DO :
-        
-        // Register your custom cell class
-        
-        
+        productsTableView.delegate = self
         // Fetch data from the database
         products = DataBaseManager.shared.getAllProducts()
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return products.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = productsTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ProductCustomCell
         let product = products[indexPath.row]
         cell.productTitle.text = product.title
+        
+        
+        if let url = URL(string: product.image) {
+                if let data = try? Data(contentsOf: url) {
+                    cell.productImage.image = UIImage(data: data)
+                }
+            }
         return cell
     }
-   
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
-
-
-
--- ProductCustomCell.swift
+++ ProductCustomCell.swift
@@ -1,14 +1,19 @@
-//
-//  ProductCustomCell.swift
-//  E-Buy (iOS)
-//
-//  Created by MacBook on 2/25/24.
-//
-
 import UIKit
 
 class ProductCustomCell: UITableViewCell {
 
     @IBOutlet var productTitle: UILabel!
+    @IBOutlet var productImage: UIImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Enable multiline for productTitle
+        productTitle.numberOfLines = 0
+        productTitle.lineBreakMode = .byWordWrapping
+        
+        // Adjust font size to fit the content
+        productTitle.adjustsFontSizeToFitWidth = true
+        productTitle.minimumScaleFactor = 0.8 // You can adjust this value as needed
+    }
 }
